f22b0151a8c6eb48a34b1c5455a0492a4d3d90ce
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2025-06-14 23:38:24 -0400

    se crea componente listar personajes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,5 +1,11 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path: 'characters',
+    loadComponent: () =>
+      import('./features/characters/character-list/character-list.component').then(
+        m => m.CharacterListComponent
+      )
+  },
 ];
