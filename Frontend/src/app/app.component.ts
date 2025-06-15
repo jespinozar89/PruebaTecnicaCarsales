d3b9b4a3a35016e2e139d62f15c25341f1dcff29
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2025-06-14 22:07:26 -0400

    Menu creado

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationMenuComponent } from "./layout/navigation-menu/navigation-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
