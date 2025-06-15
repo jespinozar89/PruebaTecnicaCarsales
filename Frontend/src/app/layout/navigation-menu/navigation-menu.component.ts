e477fd9750e7e4826cfcec07ad966d40763cb233
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2025-06-14 21:52:00 -0400

    Creacion proyecto Frontend Angular 17

---
+++ Frontend/src/app/layout/navigation-menu/navigation-menu.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navigation-menu.component.html',
+  styleUrl: './navigation-menu.component.css'
+})
+export class NavigationMenuComponent {
+
+}
