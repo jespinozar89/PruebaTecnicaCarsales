b1dca477ee27e21eb3d9c6f5f8b72be8bef1beda
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2025-06-15 23:35:44 -0400

    optimizacion de rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,22 +2,36 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
-   {
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
   {
     path: 'characters',
-    loadComponent: () =>
-      import('./features/characters/character-list/character-list.component').then(
-        m => m.CharacterListComponent
-      )
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./features/characters/character-list/character-list.component').then(
+            m => m.CharacterListComponent
+          )
+      },
+      {
+        path: ':id',
+        loadComponent: () =>
+          import('./features/characters/detail-character/detail-character.component').then(
+            m => m.DetailCharacterComponent
+          )
+      }
+    ]
   },
   {
-    path: 'characters/:id',
-    loadComponent: () =>
-      import('./features/characters/detail-character/detail-character.component').then(
-        m => m.DetailCharacterComponent
-      )
+    path: '**',
+    redirectTo: 'home'
   }
 ];
